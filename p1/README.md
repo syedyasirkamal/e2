4114cc809f6530627ffe3f4a9214e924e686e887
syedyasirkamal <syk126@g.harvard.edu>
2021-09-13 07:15:08 +0000

    Initialize Project 1

---
+++ p1/README.md
@@ -0,0 +1,10 @@
+# Project 1
+
+- By: Yasir Kamal
+- URL: <http://e2p1.kamalzadeh.me>
+
+## Game planning
+
+## Outside resources
+
+## Notes for instructor
---
+++ p1/index.php
@@ -0,0 +1,21 @@
+
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+<body>
+<h1>Project 1</h1>
+
+<h2>Mechanics</h2>
+
+<ul><li>...</li></ul>
+
+<h2>Results</h2>
+
+<ul><li>...</li></ul>
+
+</body>
+</html>
